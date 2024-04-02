3287b64fc5b8549d7262fd84ff05d8f5b6812ed3
anelsafitri <anelsafitri17@student.esaunggul.ac.id>
2024-04-02 10:39:40 +0700

    fresh from the oven

---
+++ pert3/tes.py
@@ -0,0 +1,13 @@
+value_str = input("enter value : ")
+value = int(value_str)
+if value >= 90:
+    grade = "A"
+elif value >= 80:
+    grade = "B"
+else :
+    grade = "E"
+
+print ("Grade : %s % grade")
+
+
+
